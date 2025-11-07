9b1d2cc984a383e7cbd18531067e5dccfb8b6b7c
nikhilroy22 <nikhilroyfff@gmail.com>
2025-11-07 21:43:28 +0600

    build commit

--- www/js/index.js
+++ www/js/index.js
@@ -31,3 +31,9 @@ function renderItems() {
 
 // Initial render
 renderItems();
+
+document.addEventListener('deviceready', function() {
+    // Primary color example: Blue
+    StatusBar.backgroundColorByHexString("#62f116"); // Hex code
+    StatusBar.styleLightContent(); // White text/icons
+}, false);
