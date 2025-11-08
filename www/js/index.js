4c8bff5368a7ee87064788bd34fcae71a09b3754
nikhilroy22 <nikhilroyfff@gmail.com>
2025-11-08 23:17:25 +0600

    build commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,36 +1,3 @@
-function addItem() {
-  const input = document.getElementById('item');
-  let items = JSON.parse(localStorage.getItem('items') || '[]');
-  items.push(input.value);
-  localStorage.setItem('items', JSON.stringify(items));
-  input.value = '';
-  renderItems();
-}
-
-function deleteItem(index) {
-  let items = JSON.parse(localStorage.getItem('items') || '[]');
-  items.splice(index, 1);
-  localStorage.setItem('items', JSON.stringify(items));
-  renderItems();
-}
-
-function renderItems() {
-  let items = JSON.parse(localStorage.getItem('items') || '[]');
-  const list = document.getElementById('itemList');
-  list.innerHTML = '';
-  items.forEach((item, index) => {
-    const li = document.createElement('li');
-    li.textContent = item + ' ';
-    const delBtn = document.createElement('button');
-    delBtn.textContent = 'Delete';
-    delBtn.onclick = () => deleteItem(index);
-    li.appendChild(delBtn);
-    list.appendChild(li);
-  });
-}
-
-// Initial render
-renderItems();
 
 document.addEventListener('deviceready', function() {
     // Primary color example: Blue
